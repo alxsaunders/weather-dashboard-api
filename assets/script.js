edbdd7c45b0449d14c85699d9c924d71f4f35d99
alxsaunders <alexsaunders242@gmail.com>
2022-07-06 03:27:35 -0400

    started to set up localstorage

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,20 @@ const previousSearches = document.querySelector("#previous-searches");
 const previousSearchContainer = document.querySelector("#previous-searches .card-body");
 const dailyCardContainer = document.querySelector("#daily-forecast");
 const fiveDayHeader = document.querySelector("#five-day");
+
+const localCityArray = [];
+
+// Pulls in previous searches from localStorage
+let previousSearch = JSON.parse(localStorage.getItem("searches"));
+
+// Removes any null results stored in localStorage
+if (previousSearch !== null) {
+    for (let i = 0; i < previousSearch.length; i++) {
+        if (previousSearch[i] === null) {
+            previousSearch.splice(i, i+1);
+        } else {
+            // Populates localCityArray to publish previous search buttons
+            localCityArray.push(previousSearch[i]);
+        }
+    }
+}

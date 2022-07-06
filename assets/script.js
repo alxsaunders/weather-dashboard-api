1634606734b622dbf55ab9c88b8eb696e46b00de
alxsaunders <alexsaunders242@gmail.com>
2022-07-06 03:29:20 -0400

    finish setting up local storage

--- assets/script.js
+++ assets/script.js
@@ -23,3 +23,48 @@ if (previousSearch !== null) {
         }
     }
 }
+const updateSearchHistory = () => {
+    // Pulls localStorage results of previous searches
+    previousSearch = JSON.parse(localStorage.getItem("searches"));
+
+    // Declared under function to ensure list is updated each time
+    const existingButtons = document.querySelectorAll("#previous-searches button");
+
+    if (previousSearch !== null) {
+        existingButtons.forEach(button => {
+            // Ensures buttons aren't repeated for existing searches
+            for (let i = 0; i < previousSearch.length; i++)
+            if (button.dataset.city.includes(previousSearch[i])) {
+                previousSearch.splice(i, i + 1);
+            }
+        })
+        for (let i = 0; i < previousSearch.length; i++) {
+            const searchButton = document.createElement("button");
+            searchButton.classList.add("m-2", "btn", "btn-light");
+            // Sets data-city attribute on button for event listener to reference
+            searchButton.dataset.city = previousSearch[i];
+            searchButton.textContent = previousSearch[i];
+            searchButton.addEventListener("click", (event) => {
+                // References data-city property to call API
+                callOpenWeather(event.target.dataset.city);
+            })
+            previousSearchContainer.appendChild(searchButton); 
+        }
+    }
+}
+
+const updateLocalStorage = (city) => {
+    // Ensures searched city isn't pushed into array (and then localStorage) if city has already been searched
+    if (localCityArray.includes(city)) {
+        return;
+    } else {
+        localCityArray.push(city);
+
+        // Stores for next user visit
+        localStorage.setItem("searches", JSON.stringify(localCityArray));
+        
+        // Calls updateSearchHistory to add new search to previous search buttons
+        updateSearchHistory();
+    }
+}
+
